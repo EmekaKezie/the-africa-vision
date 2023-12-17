6123d259cff6ca4bd075f3626cef2ce29b14934c
EmekaKezie <emeka.kezie@gmail.com>
2023-12-17 08:16:02 +0100

    Button Fix

--- src/component/common/PurpleButton.tsx
+++ src/component/common/PurpleButton.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@mui/material";
-import { makeStyles } from "@mui/styles";
+import { Button, createTheme } from "@mui/material";
+import { ThemeProvider, makeStyles } from "@mui/styles";
 
 type sizeTypes = "small" | "medium" | "large";
 
@@ -18,18 +18,39 @@ export default function PurpleButton(props: props) {
       startIcon={props.startIcon}
       endIcon={props.endIcon}
       size={!props.size ? "large" : props.size}
+      //variant="contained"
       //style={{}}
-      style={{
-        background: "#A8518A",
+      // style={{
+      //   background: "#A8518A",
+      //   padding: "0.8rem",
+      //   textTransform: "none",
+      //   color: "#FFFFFF",
+      //   width: "150px",
+      // }}
+      sx={{
         padding: "0.8rem",
         textTransform: "none",
         color: "#FFFFFF",
         width: "150px",
+        backgroundColor: "#A8518A",
+        "&:hover": {
+          border: "1px solid #A8518A",
+          background: "#FFFFFF",
+          color: "#A8518A",
+        },
       }}>
       {props.text}
     </Button>
   );
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#A8518A",
+    },
+  },
+});
 
 const useStyles = makeStyles(() => ({
   btn: {
