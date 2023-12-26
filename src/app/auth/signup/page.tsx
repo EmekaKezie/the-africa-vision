c8f070488960fc1d25f8312eb821a96db3217e0c
EmekaKezie <emeka.kezie@gmail.com>
2023-12-26 06:25:24 +0100

    Login

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import ReduxProvider from "@/component/common/ReduxProvider";
+import { Box } from "@mui/material";
+
+function Signup(){
+    return <Box></Box>
+}
+
+export default ReduxProvider(Signup)
